a9e3dcb563cbe302c3fe2c46e077817dc095a0b2
Jose Augusto Rocha Roque <augustorocharoque@gmail.com>
2023-10-28 14:35:01 +0000

    Criei Stgs produtos e fornecedores e a dimensão produtos.

--- models/Staging/erp_nothwind/stg_erp__categorias.sql
+++ models/Staging/erp_nothwind/stg_erp__categorias.sql
@@ -1,7 +1,7 @@
 with
-    fonte_categories as (
+    fonte_categorias as (
         select 
-            cast (category_id as int) as id_categorias
+            cast (category_id as int) as id_categoria
             ,cast (category_name as string) as nome_categoria
             ,cast (description as string) as descricao_categoria
             --,picture 
@@ -9,4 +9,4 @@ with
     )
 
     select *
-    from fonte_categories
+    from fonte_categorias
